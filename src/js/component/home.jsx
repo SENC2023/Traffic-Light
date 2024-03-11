50b9a34b2165b3158cf09e47ca2a5bc23d2d9090
SENC2023 <145979246+SENC2023@users.noreply.github.com>
2024-03-11 23:57:26 +0000

    Terminado :D

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [RedBtn, setRedBtn] = useState("bg-danger opacity-50");
+  const [YellowBtn, setYellowBtn] = useState("bg-warning opacity-50");
+  const [GreenBtn, setGreenBtn] = useState("bg-success opacity-50");
+
+  const changeColor = (color) => {
+    setRedBtn("bg-danger opacity-50");
+    setYellowBtn("bg-warning opacity-50");
+    setGreenBtn("bg-success opacity-50");
+
+    switch (color) {
+      case "red":
+        setRedBtn("bg-danger");
+        break;
+      case "yellow":
+        setYellowBtn("bg-warning");
+        break;
+      case "green":
+        setGreenBtn("bg-success");
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div>
+      <div className="cable bg-dark">a</div>
+      <div className="container bg-dark col-1 rounded-4 p-4 d-flex flex-column align-items-center">
+          <div className={" circle m-2 bg " + RedBtn} onClick={() => changeColor("red")}></div>
+          <div className={" circle m-2 bg " + YellowBtn} onClick={() => changeColor("yellow")}></div>
+          <div className={" circle m-2 bg " + GreenBtn} onClick={() => changeColor("green")}></div>
+      </div>
+    </div>
+    
+  );
 };
 
 export default Home;
